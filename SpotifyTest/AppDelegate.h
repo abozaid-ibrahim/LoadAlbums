bfaa26aed6f9b6d1fb5929ab6297c2176b7ec3f0
abuzeid ibrahim <abuzeid7@gmail.com>
2019-10-29 11:21:07 +0200

    Initial Commit

---
+++ SpotifyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotifyTest
+//
+//  Created by abuzeid on 10/29/19.
+//  Copyright © 2019 abuzeid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpotifyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotifyTest
+//
+//  Created by abuzeid on 10/29/19.
+//  Copyright © 2019 abuzeid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
